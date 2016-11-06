953cf80e96b68b5b612095242b916016d2751679
Sean Holden <seanhold3n+github@gmail.com>
2016-11-06 00:31:07 -0400

    Connected to Postgres database.

--- server.py
+++ server.py
@@ -1,7 +1,24 @@
 import os
+import psycopg2
+import urlparse
 from flask import Flask, request, Response
 
+
 app = Flask(__name__)
+
+
+# Open DB connection
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+cur = conn.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS OBDREADINGS(id SERIAL, reading JSON);")
+print('Database ready to go!')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,8 +29,17 @@ def hello_world():
 
     # Handle POST
     else:
-        print(str(request.data))
+        # Get the reading
+        reading = request.data
+
+        # Store it in the DB
+        dbquery = str.format("INSERT INTO OBDREADINGS(reading) VALUES({0});", reading)
+        cur.execute(dbquery)
+        conn.commit()
+
+        # Send okie-dokie response
         return Response(status=200)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))

870400df3efe99b7a876a80472ca3f355ec34960
Sean Holden <seanhold3n+github@gmail.com>
2016-12-18 10:56:37 -0500

    Added getConn(), some dosctring to db.py.

--- db.py
+++ db.py
@@ -25,11 +25,25 @@ print('Database ready to go!')
 
 
 def put(json_data):
+    """ Stores vehicle readings data in the database.
+
+    :param json_data:  The data as a JSON object to add to the database.  The object must contain the following keys:
+        'vehicleid', a string
+        'timestamp', the UNIX epoch time integer (as milliseconds)
+        'latitude', a float
+        'longitude', a float
+        'readings', an inner JSON object
+    :return: nothing.
+    """
     cur.execute("""INSERT INTO obdreadings(vehicleid, unix_timestamp, latitude, longitude, readings)
                 VALUES(%s, %s, %s, %s, %s);""",
                 (json_data['vehicleid'], json_data['timestamp'], json_data['latitude'], json_data['longitude'],
                  json.dumps(json_data['readings'])))
     conn.commit()
+
+
+def getConn():
+    return conn
 
 
 def getCur():
